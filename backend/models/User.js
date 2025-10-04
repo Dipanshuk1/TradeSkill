28d4f99d1c7bef40b97f62e31aea683daf71b593
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-04 14:53:14 +0530

    Updated User.js file

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"
+
+const userSchema=new mongoose.schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    bio:{
+        type:String,
+        default: ""
+    },
+    skills:[{ type: mongoose.Schema.Types.ObjectId, ref: "Skill" }],
+    profileImage:{
+        type: String,
+        default: ""
+    },
+    role:{
+        type:String,
+        enum:["student","teacher"],
+        default:"student"
+    },
+    createdAt:{
+        type:Date,
+        dafault:Date.now
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
