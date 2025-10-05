4f54342645f6437b619f8de236ed56b11ca97cb0
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-05 14:53:53 +0530

    Updated file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-Highlight;
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/",(req,res)=>{
+    res.send("this is root");
+})
+
+const PORT=process.env.PORT||4000;
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
